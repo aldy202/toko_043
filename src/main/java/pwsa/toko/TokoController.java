97e75e3333bd5a42a1834204c09d7bc08792f2e4
aldy202 <aldyfauzi202@.gmail.com>
2022-12-30 21:21:45 +0700

    add RestController

--- src/main/java/pwsa/toko/TokoController.java
+++ src/main/java/pwsa/toko/TokoController.java
@@ -8,47 +8,51 @@ package pwsa.toko;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  *
  * @author hp
  */
-@Controller
-@ResponseBody
+@RestController
 public class TokoController {
     
-    
+    //membuat object baru untuk memanggil class barang
     Barang brg = new Barang();
+    //membuat object baru untuk memanggil class barangJPAcontroller
     BarangJpaController brgJPA = new BarangJpaController();
     
     
-    @RequestMapping("/get_toko/{id}")
-    public String getBrng(@PathVariable("id") int id)
+    @GetMapping("/toko/{id}")//menggunakan request mapping berdasarkan id
+    public String getBrng(@PathVariable("id") int id)//membuat method untuk menampilkan data berdasarkan id
     {
-        try{
-            brg = brgJPA.findBarang(id);
-            String result = brg.getId().toString()+"<br>"+brg.getNama()+"<br>"+brg.getJumlah().toString();
-            return result;
+        try{//melakukan uji coba
+            brg = brgJPA.findBarang(id);//mencari id
+            String result = brg.getId().toString()+"<br>"+brg.getNama()+"<br>"+brg.getJumlah().toString();//menampilkan data
+            return result;//mengembalikan nilai result
             
-        }catch(Exception e){return "data tidak ada";}
+        }catch(Exception e){return "data tidak ada";}//menampilkan pesan jika data tidak ada
     }
     
-    @RequestMapping("/tokoall")
-    public List<Barang> getBarang()
+    @GetMapping("/toko")
+    public List<Barang> getBarang()//membuat method untuk menampilkan semua data
     {
         List<Barang> brg = new ArrayList<Barang>();
         return brgJPA.findBarangEntities();
               
     }
     
-    @RequestMapping("/delete_toko/{id}")
+    @DeleteMapping("/toko/{id}")
     public String delBarang(@PathVariable("id") int id)
             
     {
@@ -59,7 +63,7 @@ public class TokoController {
             
         
     }
-    @RequestMapping(value = "/post_toko", method = RequestMethod.POST)
+    @PostMapping("/toko")
     public String postBarang(@RequestBody Barang brg)
     {
        try{
@@ -68,7 +72,7 @@ public class TokoController {
        }catch(Exception e){return "data tidak berhasil di input";}
         
     }
-    @RequestMapping(value = "/put_toko/{id}", method = RequestMethod.PUT)
+    @PutMapping("/toko/{id}")
     public String putBarang(@RequestBody Barang barag, @PathVariable("id") int id)
     {
         try
